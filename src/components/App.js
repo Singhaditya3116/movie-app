3aecb31479f94945b8271d57b908c827e765beb6
aditya3131 <singhaditya3116@gmail.com>
2022-09-19 23:10:16 +0530

    Components added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
 
 function App() {
   return (
     <div className="App">
-      Project Setup
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab"> Movie</div>
+          <div className="tab"> Favourite</div>
+        </div>
+        <div className="movie-list">
+          <MovieCard/>
+        </div>
+      </div>
     </div>
   );
 }
